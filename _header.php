e31c4502c5003679fa0cae8da053c34b70fbdf46
Murad Rogozhnikov <murad@rogozhnikoff.com>
2015-02-25 02:54:39 +0530

    lang lenta

--- _header.php
+++ _header.php
@@ -34,6 +34,7 @@
     <link rel="stylesheet" href="css/foot-banner.css"/>
     <link rel="stylesheet" href="css/data-list.css"/>
     <link rel="stylesheet" href="css/ic.css"/>
+    <link rel="stylesheet" href="css/lang.css"/>
     <link rel="stylesheet" href="css/modlen.css"/>
 
     <script type="text/javascript" src="javascript/vendor/jquery-1.11.1.js"></script>
@@ -45,79 +46,86 @@
 <body>
 <div class="wrapper">
     <header class="head container">
-        <div class="head__theme head__theme_mb_32"></div>
+        <div class="head__theme"></div>
 
-        <div class="head__left">
-            <div class="head__logo">
-                <a href="#" class="head__logo-link"><img src='img/content/logo.svg'></a>
+        <div class="head__content">
+            <div class="head__left">
+                <div class="head__logo">
+                    <a href="#" class="head__logo-link"><img src='img/content/logo.svg'></a>
+                </div>
+                <div class="head__quality">
+                    <h1><a href="#" class="head__quality-link">Quality</a></h1>
+                    <h1><a href="#" class="head__quality-link">Beauty</a></h1>
+                    <h1><a href="#" class="head__quality-link">Accessibility</a></h1>
+                </div>
             </div>
-            <div class="head__quality">
-                <h1><a href="#" class="head__quality-link">Quality</a></h1>
-                <h1><a href="#" class="head__quality-link">Beauty</a></h1>
-                <h1><a href="#" class="head__quality-link">Accessibility</a></h1>
-            </div>
-        </div>
 
-        <div class="head__right">
-            <div class="head__top">
-                <div class="head__my">
-                    <!--<div class="head__like">-->
+            <div class="head__right">
+                <div class="head__top">
+                    <div class="head__my">
+                        <!--<div class="head__like">-->
                         <!--<div class="like-this">-->
-                            <!--<a class="like-this__heart" href=""><span class="like-this__num">0</span></a>-->
+                        <!--<a class="like-this__heart" href=""><span class="like-this__num">0</span></a>-->
                         <!--</div>-->
-                    <!--</div>-->
+                        <!--</div>-->
 
-                    <div class="head__cart">
-                        <i class="ic ic-cart head__cart-icon"></i>
-                        <a href="" class="head__cart-link">Cart</a>
-                        <span class="head__cart-count">17</span>
+                        <div class="head__cart">
+                            <i class="ic ic-cart head__cart-icon"></i>
+                            <a href="" class="head__cart-link">Cart</a>
+                            <span class="head__cart-count">17</span>
+                        </div>
+
+                        <a href="" class="head__my-link dashed"><u>My Modlen</u></a>
                     </div>
-
-                    <a href="" class="head__my-link dashed"><u>My Modlen</u></a>
+                    <div class="head__free">
+                        <a href="#" class="head__shipping-link">Free shipping</a>
+                        to New York <span class="head__and">&</span> USA
+                    </div>
                 </div>
-                <div class="head__free">
-                    <a href="#" class="head__shipping-link">Free shipping</a>
-                    to New York <span class="head__and">&</span> USA
+
+                <div class="head__navi-list">
+                    <ul class="navi-list">
+                        <li class="navi-list__item navi-list__item_m_0">
+                            <a class="navi-list__link" href="#">Rhythmic Gymnastics Leotards</a>
+                        </li>
+                        <li class="navi-list__item">
+                            <a class="navi-list__link" href="#">Acrobatic Gymnastics Dress</a>
+                        </li>
+                        <li class="navi-list__item ">
+                            <a class="navi-list__link" href="#">Ice Figure Skating Dress</a>
+                        </li>
+                    </ul>
                 </div>
-            </div>
+                <div class="head__social-list">
+                    <ul class="social-list">
+                        <li class="social-list__item social-list__item_mr_20">
+                            <a href="#" class="social-list__link">STUDIO</a>:
 
-            <div class="head__navi-list">
-                <ul class="navi-list">
-                    <li class="navi-list__item navi-list__item_m_0">
-                        <a class="navi-list__link" href="#">Rhythmic Gymnastics Leotards</a>
-                    </li>
-                    <li class="navi-list__item">
-                        <a class="navi-list__link" href="#">Acrobatic Gymnastics Dress</a>
-                    </li>
-                    <li class="navi-list__item ">
-                        <a class="navi-list__link" href="#">Ice Figure Skating Dress</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="head__social-list">
-                <ul class="social-list">
-                    <li class="social-list__item social-list__item_mr_20">
-                        <a href="#" class="social-list__link">STUDIO</a>:
-
-                        <a href="#" class="head__fb-link"><i class="icon-fb"></i></a>
-                        <a href="#" class="head__instagram-link"><i class="icon-instagram"></i></a>
-                        <a href="#" class="head__pinterest-link"><i class="icon-pinterest"></i></a>
-                    </li>
-                    <li class="social-list__item">
-                        <a href="#" class="social-list__link">STORE</a>:
-                        <a href="#" class="head__etsy-link"><i class="icon-etsy"></i></a>
-                    </li>
-                    <li class="social-list__item social-list__item_ml_20">
-                        <a href="#" class="social-list__link">HAPPY CUSTOMERS</a>
-                    </li>
-                    <li class="social-list__item social-list__item_ml_20">
-                        <a href="#" class="social-list__link">FAQ</a>
-                    </li>
-                    <li class="social-list__item social-list__item_ml_17">
-                        <a href="#" class="social-list__link">CONTACT</a>
-                    </li>
-                </ul>
+                            <a href="#" class="head__fb-link"><i class="icon-fb"></i></a>
+                            <a href="#" class="head__instagram-link"><i class="icon-instagram"></i></a>
+                            <a href="#" class="head__pinterest-link"><i class="icon-pinterest"></i></a>
+                        </li>
+                        <li class="social-list__item">
+                            <a href="#" class="social-list__link">STORE</a>:
+                            <a href="#" class="head__etsy-link"><i class="icon-etsy"></i></a>
+                        </li>
+                        <li class="social-list__item social-list__item_ml_20">
+                            <a href="#" class="social-list__link">HAPPY CUSTOMERS</a>
+                        </li>
+                        <li class="social-list__item social-list__item_ml_20">
+                            <a href="#" class="social-list__link">FAQ</a>
+                        </li>
+                        <li class="social-list__item social-list__item_ml_17">
+                            <a href="#" class="social-list__link">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
+
+        <i class="head__lang-line"></i>
+        <a class="head__lang-switcher" href="#">
+            <span class="head__lang-current"><u>en</u></span>
+        </a>
     </header>
     <div class="content container">
